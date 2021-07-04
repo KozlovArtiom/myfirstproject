f4bcfca37c7a71f0334e1a947f50541d031d773a
Artem <artiom.sar@yandex.ru>
2021-07-04 13:00:59 +0300

    Identification

--- js/index.js
+++ js/index.js
@@ -5,4 +5,9 @@ console.log(a);
 let Num = 5;
 const leftBorderWidth = 1;
 
-console.log(Num);
+//console.log(Num);
+
+let name = 'Artem';
+let age = 16;
+
+console.log(`Меня зовут ${name}, мне ${age} лет`)
